13e5beb4645c1e8decf73146b788dd81f65fd676
Filipe Pereira <pereira.lipe96@gmail.com>
2021-07-14 18:23:12 -0300

    Setup frontend

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+  root 'react#index'
+
   namespace :api do
     namespace :v1 do
       resources :notes, except: %i[new edit update]
