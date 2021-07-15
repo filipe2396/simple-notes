f3e770b900860d0aaf32deeabffd4ef41f792f5f
Filipe Pereira <pereira.lipe96@gmail.com>
2021-07-14 21:25:29 -0300

    Add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Note.create(
+  title: 'Lorem ipsum',
+  description: 'Nulla leo elit, molestie in est vitae, ullamcorper pulvinar neque. Convallis auctor massa id lobortis.'
+)
